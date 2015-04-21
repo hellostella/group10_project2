8a620c669eadc553e6e5404a4466bd801dacf4fc
Kevin E. Bauer <kevin.e.bauer@gmail.com>
2015-04-21 12:15:01 -0400

    Locations Controller Edit- Add Index View for Locations

--- project_two/db/schema.rb
+++ project_two/db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150421155113) do
   end
 
   create_table "locations", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
