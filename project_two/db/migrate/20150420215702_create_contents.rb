93aa27fae077cfc93b7f84851cc87c026ca36082
janicemin <janicemin@gmail.com>
2015-04-21 11:20:40 -0400

    Update 20150420215702_create_contents.rb

--- project_two/db/migrate/20150420215702_create_contents.rb
+++ project_two/db/migrate/20150420215702_create_contents.rb
@@ -1,9 +1,6 @@
 class CreateContents < ActiveRecord::Migration
   def change
     create_table :contents do |t|
-    	t.string :author
-    	t.string :content_url
-    	t.string :author_url
 
       t.timestamps null: false
     end
