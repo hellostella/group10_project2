80ea4d664131144ed58dbd620a3cb2f62fa804fc
JANICE MIN <JANICEMIN@GMAIL.COM>
2015-04-21 11:21:51 -0400

    deleted stuff in contents schema

--- project_two/db/migrate/20150420215702_create_contents.rb
+++ project_two/db/migrate/20150420215702_create_contents.rb
@@ -1,9 +1,7 @@
 class CreateContents < ActiveRecord::Migration
   def change
     create_table :contents do |t|
-    	t.string :author
-    	t.string :content_url
-    	t.string :author_url
+    	
 
       t.timestamps null: false
     end
