a12ee34366af06d9815d2dd0e197808fc56f4bbd
Brittany H <brittanyhenson4@gmail.com>
2015-04-23 14:04:15 -0400

    padding

--- project_two/db/schema.rb
+++ project_two/db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20150422153020) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
