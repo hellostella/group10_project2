7981d3f896f224788577cee4465ba00f57eb5a58
Kevin E. Bauer <kevin.e.bauer@gmail.com>
2015-04-23 16:34:55 -0400

    instagram api working - done

---
+++ project_two/app/models/insta.rb
@@ -0,0 +1,13 @@
+class Insta
+  
+
+  def self.location_tag (location)
+    Instagram.client_id = "03fb56bc7c104bfc86512ef66f527dd9"
+    Instagram.access_token = "6500424.03fb56b.79191adf50e44d2dbabb640d3799d2e9"
+    Instagram.tag_recent_media('location')
+  end
+
+
+
+
+end
--- project_two/app/models/tweet.rb
+++ project_two/app/models/tweet.rb
@@ -14,3 +14,12 @@ class Tweet
   end
 
 end
+
+
+class Insta
+  def self.config_two
+    client = {
+      client_id: "03fb56bc7c104bfc86512ef66f527dd9",
+      access_token: "6500424.03fb56b.79191adf50e44d2dbabb640d3799d2e9"}
+    }
+  end
